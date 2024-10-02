4a494a54e3ece965e50a579956fbc098de60ca58
karthik1772 <karthikamma2004@gmail.com>
2024-10-02 09:52:03 +0530

    completed

--- basic/swap.c
+++ basic/swap.c
@@ -3,15 +3,15 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int n,last_digit,power,first_digit,final,new;
+    int n,ld,power,fd,final,new;
+    printf("enter the number : ");
     scanf("%d",&n);
-    last_digit=n%10;
+    ld=n%10;
     power=log10(n);
-    first_digit=n/pow(10,power);
-    final=(last_digit*(int)pow(10,power))+(n%(int)pow(10,power));
-    printf("%d\n",final);
-    new=final-last_digit+first_digit;
+    fd=n/pow(10,power);
+    final=(ld*(int)pow(10,power))+(n%(int)pow(10,power));
+    new=final-ld+fd;
 
     printf("%d",new);
-    return 0;
+    return 0;
 }
---
+++ functions/seieve.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int seieve(int x);
+int main()
+{
+    int n;
+    printf("enter the last number : ");
+    scanf("%d",&n);
+
+    seieve(int n);
+}
+int seieve(int x)
+{
+    int arr(x+1);
+    
+    
+}
