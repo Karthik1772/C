1f8ad511f1ea43d490c0b7a5c63ed3821514e70d
karthik1772 <karthikamma2004@gmail.com>
2024-10-04 09:21:29 +0530

    completed

--- strings/vowconc.c
+++ strings/vowconc.c
@@ -1,20 +1,20 @@
-//Replace the vowles in 'X' and digits in 'O'
-#include<stdio.h>
+// Replace the vowles in 'X' and digits in 'O'
+#include <stdio.h>
 int main()
 {
-  char str[100];
-  int i,x;
-  
-  printf("enter :- ");
-  scanf("%s",str);
-  for(i=0;str[i];i++)
-      {
-          if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
-          str[i]='X';
-          else if(str[i]>='0' && str[i]<='9')
-          str[i] ='O';
-      }
-      printf("replace the vowles with X :- %s\n",str);
-      printf("replace the digits with O :-%s\n",str);
-      return 0;
+    char str[100];
+    int i, x;
+
+    printf("enter :- ");
+    scanf("%s", str);
+    for (i = 0; str[i]; i++)
+    {
+        if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            str[i] = 'X';
+        else if (str[i] >= '0' && str[i] <= '9')
+            str[i] = 'O';
+    }
+    printf("replace the vowles with X :- %s\n", str);
+    printf("replace the digits with O :-%s\n", str);
+    return 0;
 }
