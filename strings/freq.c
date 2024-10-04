9736891b02b523af29cffe1d0d02434248ccc852
karthik1772 <karthikamma2004@gmail.com>
2024-10-04 12:18:06 +0530

    completed

---
+++ strings/freq.c
@@ -0,0 +1,11 @@
+//to print the frequency of the string 
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int freq[26]={0};
+    printf("enter the string : ");
+    scanf("%[^\n]s",str);
+
+    
+}
