0ab1a5440485deca486f482ef984e0d7a149beb9
karthik1772 <karthikamma2004@gmail.com>
2024-10-07 12:02:52 +0530

    completed

---
+++ recursion/reverse.c
@@ -0,0 +1,19 @@
+//write a program to reverse a string using recursion
+#include<stdio.h>
+void reverse(char *p)
+{
+    if(*p!='\0')
+    {
+        reverse(p+1);
+        putchar(*p);
+    }
+}
+int main()
+{
+    char s1[100];
+    printf("Enter the string  : ");
+    scanf("%s",s1);
+    reverse(s1);
+
+    return 0;
+}
